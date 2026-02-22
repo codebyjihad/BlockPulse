63c4e7e5dfc4137f316155d453d48f38dace90cf
jihad <codebyjihad@gmail.com>
2026-02-22 15:48:08 +0600

    feat: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react'
 import Logo from './Logo'
 import { navItems } from './data/navitems'
-import {  NavLink } from 'react-router'
+import { NavLink } from 'react-router'
 import { FaMoon, FaSun } from "react-icons/fa";
-import { IoMdClose , IoMdMenu } from "react-icons/io";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
 
 
 import Search from './Search'
 
 const Navbar = () => {
-   
+
     const [isMenuOpen, setIsmenuOpen] = useState(false)
-    
-     const toggleMobileMenu = () => {
+
+    const toggleMobileMenu = () => {
         setIsmenuOpen(!isMenuOpen)
-     }
+    }
 
 
     const [isDarkMode, setIsDarkMode] = useState(false)
@@ -62,15 +62,30 @@ const Navbar = () => {
                 <div className='md:hidden block'>
                     <button onClick={toggleMobileMenu} className='cursor-pointer'>
                         {
-                            isMenuOpen? <IoMdClose className='size-6 text-gray-600'/>:<IoMdMenu className='size-6 text-gray-600'/>
+                            isMenuOpen ? <IoMdClose className='size-6 text-gray-600' /> : <IoMdMenu className='size-6 text-gray-600' />
                         }
                     </button>
                 </div>
             </div>
             {/* mobile menu items */}
-            <div className='hidden'>
-                
-            </div>
+            {
+                isMenuOpen && <div className='md:hidden bg-white shadow-md'>
+                    <ul className='flex flex-col items-center  space-y-4 text-gray-800 py-4'>
+                        {
+                            navItems.map((items) => (
+                                <li onClick={toggleMobileMenu} key={items.id}><NavLink className={({ isActive }) =>
+                                    `font-medium transition ${isActive
+                                        ? "text-secondary border-b-2 border-secondary"
+                                        : "text-primary hover:text-secondary"
+                                    }`
+                                }
+                                    to={items.to}>{items.name}</NavLink></li>
+                            ))
+                        }
+                    </ul>
+                    <Search/>
+                </div>
+            }
         </nav>
     )
 }
