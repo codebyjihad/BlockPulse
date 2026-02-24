880882aec0136c0251c461f28b0babb2944a2334
jihad <codebyjihad@gmail.com>
2026-02-24 12:02:25 +0600

    fetching data

--- src/pages/blog/BlogList.jsx
+++ src/pages/blog/BlogList.jsx
@@ -7,11 +7,21 @@ const BlogList = () => {
         fetch('blogsdata.json')
         .then(response => response.json())
         .then(data => setBlogs(data))
+        .catch(error => console.error("Error fetching blog data" + error))
     },[])
+
 
   return (
     <div className='container mx-auto'>
-
+          <div>
+            {
+                blogs.map((blog , index) => (
+                    <div key={index}>
+                          <h1>{blog.title}</h1>
+                    </div>
+                ))
+            }
+          </div>
     </div>
   )
 }
