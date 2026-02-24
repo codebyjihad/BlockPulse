b2e629d80ce48d1b81f9b83cac5c51820a3f3606
jihad <codebyjihad@gmail.com>
2026-02-24 14:50:13 +0600

    feat:create footeer section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import BlogList from '../blog/BlogList'
+import NewsLetter from './NewsLetter'
 
 const Home = () => {
   return (
     <section className='container max-w-7xl mx-auto px-4 py-3'>
           <Banner/>
           <BlogList/>
+          <NewsLetter/>
     </section>
   )
 }
